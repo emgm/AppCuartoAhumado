58228c6e7b30c59dce344edb3e3d3c9a36638df3
DESARROLLO-07 <emgonzalezm@ufpso.edu.co>
2018-12-11 08:28:28 -0500

    Commit

--- app/src/main/java/co/edu/ufpso/appcuartoahumado/SplashScreen.java
+++ app/src/main/java/co/edu/ufpso/appcuartoahumado/SplashScreen.java
@@ -1,5 +1,6 @@
 package co.edu.ufpso.appcuartoahumado;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,24 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__screen);
+
+        Thread myThread = new Thread() {
+
+            public void run() {
+
+                try {
+                    sleep(4000);
+
+                    startActivity(new Intent(getApplicationContext(), Principal.class));
+                    finish();
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+
+
     }
 }
